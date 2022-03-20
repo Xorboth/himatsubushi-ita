42b411e87bdead8b850ef601b511995844ea6ed6
Xorboth <lesmaulan@gmail.com>
2022-03-20 13:36:59 +0100

    Update build script

--- build/build_archives.bat
+++ build/build_archives.bat
@@ -2,7 +2,7 @@
 :: Set variables
 ::
 
-SET /P version= Enter the version number: 
+set /P version= Enter the version number: 
 
 set tempFolder=temp
 set outputFolder=output
@@ -64,7 +64,7 @@ copy ..\%readmeManual% .\%tempFolder%\%readme%
 
 
 ::
-:: Create Windows Steam GOG manual archive
+:: Create Windows Steam/GOG/MangaGamer manual archive
 ::
 
 del .\%tempFolder%\%higurashiData%\%assets%
@@ -77,7 +77,7 @@ copy ..\%higurashiData%\%assetsWindowsSteamGogMangaGamer% .\%tempFolder%\%higura
 
 
 ::
-:: Create Linux/MacOS Steam GOG manual archive
+:: Create Linux/MacOS Steam/GOG/MangaGamer manual archive
 ::
 
 del .\%tempFolder%\%higurashiData%\%assets%
